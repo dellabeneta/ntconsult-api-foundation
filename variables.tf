7fc51767fec26b5b3eb8eff7275b7479fa3cc316
dellabeneta <m.dellabeneta@gmail.com>
2024-04-09 14:22:16 -0300

    Ajustes no readme.md

--- variables.tf
+++ variables.tf
@@ -19,17 +19,22 @@ variable "bucket_backend_name" {
 variable "project_name" {
   default = "API Foundation"
   description = "Nome do projeto dentro da Digital Ocean apenas"
-  type = string
+  type    = string
 }
 
 variable "project_purpose" {
   default = "Service or API"
   description = "Proposta desse projeto"
-  type = string
+  type    = string
 }
 
 variable "project_environment" {
   default = "Production"
   description = "Tipo de Ambiente que estamos lidando 'dev', 'prd' ..."
+  type    = string
+}
+
+variable "project_description" {
+  default = "Esse projeto agrupa os recursos criados pela IaC Foundation."
   type    = string
 }
