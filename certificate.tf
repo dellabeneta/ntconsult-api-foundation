57e1444bfed19b4351c5559bf3b518cf7556a6dc
dellabeneta <m.dellabeneta@gmail.com>
2024-04-15 19:34:01 -0300

    Criando pipeline v1 para testes

--- certificate.tf
+++ certificate.tf
@@ -1,7 +1,7 @@
-# resource "digitalocean_certificate" "cert" {
-#   name    = "dellabeneta-wildcard-cert"
-#   type    = "lets_encrypt"
-#   domains = ["*.dellabeneta.online"]
+resource "digitalocean_certificate" "cert" {
+  name    = "dellabeneta-wildcard-cert"
+  type    = "lets_encrypt"
+  domains = ["*.dellabeneta.online"]
 
-#   depends_on = [ digitalocean_domain.domain ]
-# }
+  depends_on = [ digitalocean_domain.domain ]
+}
